7d68f88e8ed05052d93cb1e23e207bc36493f208
Artcas2 <63158439+Artcas2@users.noreply.github.com>
2026-05-25 17:30:57 +0200

    Fix build script

--- build.bat
+++ build.bat
@@ -1,1 +1,14 @@
+@echo off
+if exist venv (
+  call venv\Scripts\activate.bat
+) else (
+  if not exist .venv (
+    echo Creation d'un environnement virtuel...
+    call python -m venv .venv
+  )
+  call .venv\Scripts\activate.bat
+)
+
+pip install -r requirements-dev.txt
 pyinstaller --onefile --icon=icon.ico install_tools.py
+pause
